ec68403d02ff747e468aa7d1b0e364391193a7f4
heeheejj <kjyk0835@naver.com>
2022-08-03 01:32:45 +0900

    modify BOJ_9012: 괄호

--- src/heeheejj/week03/BOJ_9012.java
+++ src/heeheejj/week03/BOJ_9012.java
@@ -1,4 +1,4 @@
-package heeheejj.week03;
+﻿package heeheejj.week03;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -10,20 +10,22 @@ import java.util.Stack;
 public class BOJ_9012 {
     static String isRightBrk(char[] arr){
         Stack<Character> stack = new Stack<>();
-        for(char brk: arr){
-            if(brk == '('){
+        for(char brk: arr){	// 문자 배열의 원소를 하나씩 확인
+            if(brk == '('){	// 괄호가 열리면 Stack에 쌓는다.
                 stack.push('(');
-            }else if(brk == ')'){
-                if(!stack.isEmpty()){
+            }else if(brk == ')'){	// 괄호가 닫히면
+                if(!stack.isEmpty()){	// 괄호가 닫혔고, 스택이 비어있지 않다면 스택에서 하나 꺼낸다.
                     stack.pop();
                 }else{
-                    return "NO";
+                    return "NO";	// 괄호가 닫혔는데 스택이 비어있다면 VPS(Valid Ps, 올바른 괄호 문자열)가 아니다.
                 }
             }
         }
-        if(stack.empty()){
+
+        // 문자 배열을 모두 확인하고 나서
+        if(stack.empty()){	 // 스택이 비어있다면 VPS가 맞다.
             return "YES";
-        }else{
+        }else{		// 스택이 비어있지 않다면 VPS가 아니다.
             return "NO";
         }
     }
